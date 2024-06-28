99e492151692c420e4261b3ad60382610fd909f6
mayckon <mayckonalexandre000@gmail.com>
2024-06-27 22:11:50 -0300

    ajuste

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import { router } from "./route";
+import morganBody from "morgan-body";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+morganBody(app);
 app.use(express.static("public"));
 app.use(router);
 app.listen(process.env.PORT, () =>
